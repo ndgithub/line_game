1103f96816464b4dc5770045f09126badb7befdc
ndgithub <nickydesai.mail@gmail.com>
2018-12-10 19:15:51 -0600

    Messes everthing up

--- src/App.js
+++ src/App.js
@@ -54,7 +54,6 @@ class App extends Component {
           </div>
         </div>
       </div>
-
     );
   }
   handleWindowResize = () => {
--- src/Guesses.js
+++ src/Guesses.js
@@ -2,28 +2,63 @@ import React from 'react';
 import Record from './Record.js';
 
 
+class Guesses extends React.Component {
 
-const Guesses = (props) => {
-	
-	const guesses = props.guessList.map((record,index) => {
-			return <Record actual = {record.actual} 
-										 guess = {record.userGuess} 
-										 key = {index} />
+
+	constructor(props) {
+		super(props);
+	}
+
+
+	const guesses = props.guessList.map((record, index) => {
+		return <Record actual={record.actual}
+			guess={record.userGuess}
+			key={index} />
 	});
-	
-	const error = props.guessList.reduce((totalError,record) => {
-			return totalError + Math.abs(record.actual - record.userGuess);
-		},0)
-	
+
+	const error = props.guessList.reduce((totalError, record) => {
+		return totalError + Math.abs(record.actual - record.userGuess);
+	}, 0)
+
 	let currentGuess = guesses.length < 5 ? guesses.length + 1 : 5;
+renderTable = () => {
 	return (
-		<div className="stats">
-			<div className="guess_list">{guesses}</div>
-			<div className="counter">{currentGuess + " out of 5"}</div>
-			<div className = "total_error">{`Total Error: ${error}`}</div>
-		</div>
-		); 
+		<table>
+			<tr>
+				<td>Actual</td>
+				<td>You</td>
+				<td>Off By</td>
+			</tr>
+			{guesses.map(guessObject => {
+				return guessObject.actual
+			})}
+		</table>
+	)
 }
+
+render() {
+
+(
+	<div className="stats">
+		<div className="record_container">
+			<div className="record_entry">
+				<p>Actual</p>
+			</div>
+			<div className="record_entry">
+				<p>You</p>
+			</div>
+			<div className="record_entry">
+				<p>Off by:</p>
+			</div>
+		</div>
+
+		<div className="guess_list">{guesses.reverse()}</div>
+
+		<div className="counter">{currentGuess + " out of 5"}</div>
+		<div className="total_error">{`Total Error: ${error}`}</div>
+	</div>
+);}
+
 
 
 export default Guesses;
